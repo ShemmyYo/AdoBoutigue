fbc82ea104ac800dd96747750fa451e6a149ae16
ShemmyYo <rabiliamemo@gmail.com>
2023-09-28 20:15:11 +0000

    added Product fields to admin

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 from .models import Product, Category, Condition, AgeGroup
 
 # Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Condition)
 admin.site.register(AgeGroup)
