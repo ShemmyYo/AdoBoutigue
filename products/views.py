b6ec582c81de54c399e3cbe9f658b9e664d7302f
ShemmyYo <rabiliamemo@gmail.com>
2023-09-30 08:20:49 +0000

    added categories and sorthing methods

--- products/views.py
+++ products/views.py
@@ -1,28 +1,38 @@
 from django.shortcuts import (render, redirect, reverse, get_object_or_404)
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
     """
-    A view to return all products
+    A view to return all products, product queries and sorting.
     """
     products = Product.objects.all()
+    query = None
+    category = None
 
-    if 'q' in request.GET:
-        query = request.GET['q']
-        if not query:
-            messages.error(request, "You didn't enter\
-                            any search criteria!")
-            return redirect(reverse('products'))
-        
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
+    if request.GET:
+        if 'category' in request.GET:
+                categories = request.GET['category'].split(',')
+                products = products.filter(category__name__in=categories)
+                categories = Category.objects.filter(name__in=categories)
+                
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter\
+                                any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query, 
+        'current_categories': category,
     }
 
     return render(request, 'products/products.html', context)
