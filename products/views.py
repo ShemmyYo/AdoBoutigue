40a954da337119f4caf482e82f190262762f763f
ShemmyYo <rabiliamemo@gmail.com>
2023-09-30 09:30:55 +0000

    changed Product view, category

--- products/views.py
+++ products/views.py
@@ -33,7 +33,7 @@ def all_products(request):
         if 'category' in request.GET:
                 categories = request.GET['category'].split(',')
                 products = products.filter(category__name__in=categories)
-                categories = Category.objects.filter(name__in=categories)
+                category = Category.objects.filter(name__in=categories)
                 
         if 'q' in request.GET:
             query = request.GET['q']
