3b4d9f5c4f28b8e3292793c9f07e6b65bbafb42d
ShemmyYo <rabiliamemo@gmail.com>
2023-10-19 19:03:01 +0000

    stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51O30vcGG8Vrt181dNdGUmnknm0KpXPzqeiHMHhkKziPa5oxj5jSpbLC1TluGgbMA1zhuBixGPEm8SCs5aCeytx5b00ALJyltr3',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
