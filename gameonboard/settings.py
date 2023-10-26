7fa2828823d0d1afb1c13f33404659e5fb38d21a
ShemmyYo <rabiliamemo@gmail.com>
2023-10-26 09:31:43 +0000

    secret key created

--- gameonboard/settings.py
+++ gameonboard/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yfd4dj=04^7#@_i(be08j5y5*c3-9di9df!i*ogd@fs^mqj34k'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' is os.environ
 
 ALLOWED_HOSTS = [
     'gamer-on-board-6fa9b306b6d7.herokuapp.com',
