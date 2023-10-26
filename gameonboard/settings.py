fdae53e276035a40aa5d1988ef7e0a331e24eb9b
ShemmyYo <rabiliamemo@gmail.com>
2023-10-26 08:04:55 +0000

    Deployment to Heroku

--- gameonboard/settings.py
+++ gameonboard/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,11 @@ SECRET_KEY = 'django-insecure-yfd4dj=04^7#@_i(be08j5y5*c3-9di9df!i*ogd@fs^mqj34k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-shemmyyo-gameonboardp5e-icarjpxt1os.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = [
+    'gamer-on-board.herokuapp.com', 
+    'localhost', 
+    '8000-shemmyyo-gameonboardp5e-icarjpxt1os.ws-eu105.gitpod.io'
+    ]
 
 
 # Application definition
@@ -121,12 +126,28 @@ WSGI_APPLICATION = 'gameonboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://ikpqrqdk:fh6bPhsx7Vv7T6vacK3HkqOXBZxwuBA4@tyke.db.elephantsql.com/ikpqrqdk')
+# }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
