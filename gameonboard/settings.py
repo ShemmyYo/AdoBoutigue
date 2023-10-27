0d798bcf0a1a71aeffb20d84a8c3dc78c8333132
ShemmyYo <rabiliamemo@gmail.com>
2023-10-27 10:31:31 +0000

    added home in mobile and style changes

--- gameonboard/settings.py
+++ gameonboard/settings.py
@@ -24,8 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' is os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     'gamer-on-board-6fa9b306b6d7.herokuapp.com',
