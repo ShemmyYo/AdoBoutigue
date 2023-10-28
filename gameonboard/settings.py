15991ba24f46b9b2ab3c4deb30f4c41c380422bd
ShemmyYo <rabiliamemo@gmail.com>
2023-10-28 08:30:56 +0000

    db migration

--- gameonboard/settings.py
+++ gameonboard/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'l#l1&or^2mv=436f_ew593mz6d6i7-cy26n2h3x$ba%rj$tfj2')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- products/models.py
+++ products/models.py
@@ -10,7 +10,6 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     cat_desc = models.CharField(max_length=254, null=True, blank=True)
-    cat_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
