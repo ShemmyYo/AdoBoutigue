0f7b2e00a56601dd4d549b65b5cb0830d7d31bb7
ShemmyYo <rabiliamemo@gmail.com>
2023-10-28 14:28:12 +0000

    added stock adjustment on purchase

--- checkout/views.py
+++ checkout/views.py
@@ -68,6 +68,11 @@ def checkout(request):
                             product=product,
                             quantity=item_data,
                         )
+
+                        # Reduce stock on purchase
+                        product.stock = product.stock - order_line_item.quantity
+                        product.save()
+
                         order_line_item.save()
                     else:
                         for size, quantity in item_data['items_by_size'].items():
