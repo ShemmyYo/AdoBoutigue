b9c8e4c0861d54ffb65562288ef9801a2cca3e42
ShemmyYo <rabiliamemo@gmail.com>
2023-10-28 16:01:34 +0000

    quantity managment on product details

--- products/views.py
+++ products/views.py
@@ -45,7 +45,8 @@ def all_products(request):
                                 any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -87,7 +88,8 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product. Please ensure the \
+            form is valid.')
     else:
         form = ProductForm()
 
@@ -114,7 +116,8 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to update product. Please ensure \
+            the form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -134,6 +137,7 @@ def delete_product(request, product_id):
     if not request.user.is_superuser:
         messages.error(request, 'You have no access to this function')
         return redirect(reverse('home'))
+        
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
