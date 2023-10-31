0fd41c57355321c3d03d645c691a826c9868dc5d
ShemmyYo <rabiliamemo@gmail.com>
2023-10-31 12:18:50 +0000

    added 2 additional rows to Product model

--- products/models.py
+++ products/models.py
@@ -60,6 +60,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     stock = models.IntegerField(default=1)
 
     def __str__(self):
