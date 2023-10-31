861f253e4b8b910dbb0a14a184a467ab0c3bd7b4
ShemmyYo <rabiliamemo@gmail.com>
2023-10-31 12:19:52 +0000

    changed model field names

--- products/models.py
+++ products/models.py
@@ -60,8 +60,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image2 = models.ImageField(null=True, blank=True)
+    image3 = models.ImageField(null=True, blank=True)
     stock = models.IntegerField(default=1)
 
     def __str__(self):
