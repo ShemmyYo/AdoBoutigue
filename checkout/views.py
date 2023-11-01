747550f956f88fe4b900c0fce709f8a34e1c7321
ShemmyYo <rabiliamemo@gmail.com>
2023-11-01 20:24:56 +0000

    removed defer from script links

--- checkout/views.py
+++ checkout/views.py
@@ -71,14 +71,12 @@ def checkout(request):
                         )
 
                         # Reduce stock on purchase
-                        product.stock = product.stock - order_line_item.\
-                            quantity
+                        product.stock = product.stock - order_line_item.quantity
                         product.save()
 
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].\
-                                items():
+                        for size, quantity in item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -96,8 +94,7 @@ def checkout(request):
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success',\
-                args=[order.order_number]))
+            return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -106,8 +106,7 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS:\
-                        Verified order already in database',
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
         else:
             order = None
@@ -136,8 +135,7 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'\
-                                ].items():
+                        for size, quantity in item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
